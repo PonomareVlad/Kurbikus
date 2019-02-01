adad066bbdb9219225585b567d928b649809f21c
PonomareVlad <sys.system@mail.ru>
2019-02-01 10:04:47 +0500

    Render country slides and switching between

--- assets/js/contacts.js
+++ assets/js/contacts.js
@@ -1,7 +1,8 @@
 export default class Contacts {
-    constructor({dataUrl = 'contacts.json'} = {}) {
+    constructor({dataUrl = 'contacts.json', desktop = true} = {}) {
         this.parameters = {
-            dataUrl
+            dataUrl,
+            desktop
         };
         this.init();
         return this;
@@ -9,6 +10,7 @@ export default class Contacts {
 
     async init() {
         await this.loadContactsData();
+        this.renderCountriesSlider();
         this.renderCountriesMenu();
     }
 
@@ -19,6 +21,7 @@ export default class Contacts {
 
     renderCountriesMenu() {
         const menuNode = document.querySelector('#countriesMenu');
+        menuNode.onchange = event => this.selectCountry(event.target.value);
         menuNode.innerHTML = '';
         this.countries.forEach(country => {
             const optionNode = document.createElement('option');
@@ -28,7 +31,32 @@ export default class Contacts {
         })
     }
 
-    getL10n(value, language) {
+    renderCountriesSlider() {
+        const sliderNode = document.querySelector('.contacts-slider');
+        sliderNode.innerHTML = '';
+        this.countries.forEach(country => {
+            const slideNode = document.createElement('div');
+            slideNode.className = 'country-card';
+            slideNode.dataset.country = country[0];
+            slideNode.innerHTML = `<h3 class="title">${this.getL10n(country[1].title)}</h3><div class="country-content">
+<div class="cities-section"></div><div class="contacts-details"><div><h4 class="label">Выберете город из списка</h4></div>
+<a class="show-feedback-button" href="#feedback">Оставить заявку</a></div></div>`;
+            sliderNode.appendChild(slideNode);
+        })
+    }
+
+    selectCountry(country) {
+        if (this.parameters.desktop) this.scrollToCountry(country);
+    }
+
+    scrollToCountry(country) {
+        document.querySelector('.contacts-slider').scrollTo({
+            left: document.querySelector(`.country-card[data-country="${country}"]`).offsetLeft - 160,
+            behavior: 'smooth'
+        });
+    }
+
+    getL10n(value, language = 'ru') {
         if (typeof value !== "object") return value;
         if (value[language]) return value[language];
         return Object.values(value)[0];
