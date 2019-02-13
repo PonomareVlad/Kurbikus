19852135f9eba39436ea679ea6fea8d8c53fc7b1
PonomareVlad <sys.system@mail.ru>
2019-02-13 06:47:18 +0500

    Desktop map prototype

--- assets/js/contacts.js
+++ assets/js/contacts.js
@@ -1,65 +1,65 @@
 export default class Contacts {
-	constructor({dataUrl = 'contacts.json', desktop = true} = {}) {
-		this.parameters = {
-			dataUrl,
-			desktop
-		};
-		this.init();
-		return this;
-	}
+    constructor({dataUrl = 'contacts.json', desktop = true} = {}) {
+        this.parameters = {
+            dataUrl,
+            desktop
+        };
+        this.init();
+        return this;
+    }
 
-	async init() {
-		await this.loadContactsData();
-		this.renderCountriesSlider();
-		this.renderCountriesMenu();
-	}
+    async init() {
+        await this.loadContactsData();
+        this.renderCountriesSlider();
+        this.renderCountriesMenu();
+    }
 
-	async loadContactsData() {
-		this.contactsData = await fetch(this.parameters.dataUrl).then(response => response.json());
-		this.countries = Object.entries(this.contactsData);
-		this.countries.forEach(country => country[1].citiesList = Object.entries(country[1].cities));
-	}
+    async loadContactsData() {
+        this.contactsData = await fetch(this.parameters.dataUrl).then(response => response.json());
+        this.countries = Object.entries(this.contactsData);
+        this.countries.forEach(country => country[1].citiesList = Object.entries(country[1].cities));
+    }
 
-	renderCountriesMenu() {
-		const menuNode = document.querySelector('#countriesMenu');
-		menuNode.onchange = event => this.selectCountry(event.target.value);
-		menuNode.innerHTML = '';
-		this.countries.forEach(country => {
-			const optionNode = document.createElement('option');
-			optionNode.value = country[0];
-			optionNode.innerText = this.getL10n(country[1].title);
-			menuNode.appendChild(optionNode);
-		})
-	}
+    renderCountriesMenu() {
+        const menuNode = document.querySelector('#countriesMenu');
+        menuNode.onchange = event => this.selectCountry(event.target.value);
+        menuNode.innerHTML = '';
+        this.countries.forEach(country => {
+            const optionNode = document.createElement('option');
+            optionNode.value = country[0];
+            optionNode.innerText = this.getL10n(country[1].title);
+            menuNode.appendChild(optionNode);
+        })
+    }
 
-	renderCountriesSlider() {
-		const sliderNode = document.querySelector('.contacts-slider');
-		sliderNode.innerHTML = '';
-		this.countries.forEach(country => {
-			const slideNode = document.createElement('div');
-			slideNode.className = 'country-card';
-			slideNode.dataset.country = country[0];
-			slideNode.innerHTML = `<h3 class="title">${this.getL10n(country[1].title)}</h3><div class="country-content">
+    renderCountriesSlider() {
+        const sliderNode = document.querySelector('.contacts-slider');
+        sliderNode.innerHTML = '';
+        this.countries.forEach(country => {
+            const slideNode = document.createElement('div');
+            slideNode.className = 'country-card';
+            slideNode.dataset.country = country[0];
+            slideNode.innerHTML = `<h3 class="title">${this.getL10n(country[1].title)}</h3><div class="country-content">
 <div class="cities-section"></div><div class="contacts-details"><div><h4 class="label">Выберете город из списка</h4></div>
 <a class="show-feedback-button" href="#feedback">Оставить заявку</a></div></div>`;
-			const citiesSectionNode = slideNode.querySelector('.cities-section');
-			citiesSectionNode.innerHTML = '';
-			country[1].citiesList.forEach(city => {
-				const cityLinkNode = document.createElement('a');
-				cityLinkNode.innerText = this.getL10n(city[1].title);
-				cityLinkNode.onclick = event => this.renderCity(country[0], city[0]);
-				cityLinkNode.classList.toggle('primary-city', city[1]['primary']);
-				citiesSectionNode.appendChild(cityLinkNode);
-			});
-			sliderNode.appendChild(slideNode);
-		})
-	}
+            const citiesSectionNode = slideNode.querySelector('.cities-section');
+            citiesSectionNode.innerHTML = '';
+            country[1].citiesList.forEach(city => {
+                const cityLinkNode = document.createElement('a');
+                cityLinkNode.innerText = this.getL10n(city[1].title);
+                cityLinkNode.onclick = event => this.renderCity(country[0], city[0]);
+                cityLinkNode.classList.toggle('primary-city', city[1]['primary']);
+                citiesSectionNode.appendChild(cityLinkNode);
+            });
+            sliderNode.appendChild(slideNode);
+        })
+    }
 
-	renderCity(country, city) {
-		const cityData = this.contactsData[country].cities[city];
-		if (this.parameters.desktop) {
-			const detailsNode = document.querySelector(`.country-card[data-country="${country}"]`).querySelector('.contacts-details');
-			detailsNode.innerHTML = `
+    renderCity(country, city) {
+        const cityData = this.contactsData[country].cities[city];
+        if (this.parameters.desktop) {
+            const detailsNode = document.querySelector(`.country-card[data-country="${country}"]`).querySelector('.contacts-details');
+            detailsNode.innerHTML = `
 			<div>
 				<h4 class="label">Адрес:</h4>
 				<p class="caption">${this.getL10n(cityData.address)}</p>
@@ -72,30 +72,52 @@ export default class Contacts {
 				<h4 class="label">Email</h4>
 				<a class="caption" href="mailto:${this.getL10n(cityData.email)}">${this.getL10n(cityData.email)}</a>
 			</div>
-			<a class="show-feedback-button" href="#feedback">Оставить заявку</a>
+			<div class="buttons-row">
+			    <a class="show-feedback-button" href="#feedback">Оставить заявку</a>
+			    <button class="show-map-button">Показать карту</button>
+			</div>
 			<div class="social-icons">
 				<a class="vk-icon" href="#"></a>
 				<a class="fb-icon" href="#"></a>
 				<a class="yt-icon" href="#"></a>
 				<a class="ok-icon" href="#"></a>
-			</div>`
-		}
-	}
+			</div>`;
+            detailsNode.querySelector('.show-map-button').addEventListener('click', () => this.renderCityMap(country, city));
+        }
+    }
 
-	selectCountry(country) {
-		if (this.parameters.desktop) this.scrollToCountry(country);
-	}
+    renderCityMap(country, city) {
+        debugger;
+        const cityData = this.contactsData[country].cities[city];
+        const oldMapWrapperNode = document.querySelector(`.country-card[data-country="${country}"]`).querySelector('.map-wrapper');
+        if (oldMapWrapperNode) oldMapWrapperNode.parentNode.removeChild(oldMapWrapperNode);
+        const mapWrapperNode = document.createElement('div');
+        mapWrapperNode.className = 'map-wrapper';
+        mapWrapperNode.innerHTML = `<h3 class="title">${this.getL10n(cityData.title)}</h3>
+<iframe src="${cityData.mapUrl}" width="100%" height="100%" frameborder="0"></iframe><button class="map-close-button">✕</button>`;
+        document.querySelector(`.country-card[data-country="${country}"]`).appendChild(mapWrapperNode);
+        mapWrapperNode.querySelector('.map-close-button').addEventListener('click', () => this.destroyCityMap(country));
+    }
 
-	scrollToCountry(country) {
-		document.querySelector('.contacts-slider').scrollTo({
-			left: document.querySelector(`.country-card[data-country="${country}"]`).offsetLeft - 160,
-			behavior: 'smooth'
-		});
-	}
+    destroyCityMap(country) {
+        const oldMapWrapperNode = document.querySelector(`.country-card[data-country="${country}"]`).querySelector('.map-wrapper');
+        if (oldMapWrapperNode) oldMapWrapperNode.parentNode.removeChild(oldMapWrapperNode);
+    }
 
-	getL10n(value, language = 'ru') {
-		if (typeof value !== "object") return value;
-		if (value[language]) return value[language];
-		return Object.values(value)[0];
-	}
+    selectCountry(country) {
+        if (this.parameters.desktop) this.scrollToCountry(country);
+    }
+
+    scrollToCountry(country) {
+        document.querySelector('.contacts-slider').scrollTo({
+            left: document.querySelector(`.country-card[data-country="${country}"]`).offsetLeft - 160,
+            behavior: 'smooth'
+        });
+    }
+
+    getL10n(value, language = 'ru') {
+        if (typeof value !== "object") return value;
+        if (value[language]) return value[language];
+        return Object.values(value)[0];
+    }
 }
